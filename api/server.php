951e7c14268bdfe38b7afdcd126024978d55db59
Tech4Ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2023-06-21 16:44:39 +0100

    Update server.php

--- api/server.php
+++ api/server.php
@@ -1,64 +1,23 @@
 <?php
 
-header("Access-Control-Allow-Origin: 'http://127.0.0.1:5500/index.html'");
+header("Access-Control-Allow-Origin: 'http://127.0.0.1:5500/'");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 60");    // cache for 1 minute
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: *");
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = file_get_contents('php://input');
+$url = "https://g82.me/t?p=". $_GET['s'];
 
-    $url = "https://g82.me/t";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-    $headers = array(
-        "Content-Type: application/json",
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-
-    // For debug only!
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-    $resp = curl_exec($curl);
-    curl_close($curl);
-
-    if ($resp === false) {
-        $response = array(
-            'status' => 'error',
-            'message' => 'Failed to make the request.'
-        );
-    } else {
-        $decodedResp = json_decode($resp, true);
-
-        if ($decodedResp === null && json_last_error() !== JSON_ERROR_NONE) {
-            $response = array(
-                'status' => 'error',
-                'message' => 'Failed to decode the response JSON. Error: ' . json_last_error_msg()
-            );
-        } else {
-            $response = array(
-                'status' => 'success',
-                'message' => $decodedResp
-            );
-        }
-    }
-} else {
-    $response = array(
-        'status' => 'error',
-        'message' => 'Invalid request method. Only POST requests are allowed.'
-    );
-}
-
-// Send the response as JSON
-header('Content-Type: application/json');
-echo json_encode($response);
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
 ?>
