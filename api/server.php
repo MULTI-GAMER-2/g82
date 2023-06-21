f85cb327bf4d5ffd689e8783987a41827cade5d5
Tech4Ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2023-06-21 13:13:55 +0100

    Update server.php

--- api/server.php
+++ api/server.php
@@ -38,54 +38,54 @@ $response = array();
 
 // Check if the required parameters are present
 if (isset($data['token']) && isset($data['p'])) {
-  $token = $data['token'];
-  $p = $data['p'];
+    $token = $data['token'];
+    $p = $data['p'];
 
-  // Extract the timestamp from the token
-  $timestamp = substr($token, -13);
+    // Extract the timestamp from the token
+    $timestamp = substr($token, -13);
 
-  // Convert the timestamp to a DateTime object
-  $datetime = DateTime::createFromFormat('U', floor(intval($timestamp) / 1000));
+    // Convert the timestamp to a DateTime object
+    $datetime = DateTime::createFromFormat('U', floor(intval($timestamp) / 1000));
 
-  // Calculate the time difference between the current time and the token's timestamp
-  $currentDatetime = new DateTime();
-  $timeDiff = $currentDatetime->getTimestamp() - $datetime->getTimestamp();
+    // Calculate the time difference between the current time and the token's timestamp
+    $currentDatetime = new DateTime();
+    $timeDiff = $currentDatetime->getTimestamp() - $datetime->getTimestamp();
 
-  // Set the time limit in seconds (5 seconds in this example)
-  $timeLimit = 5;
+    // Set the time limit in seconds (5 seconds in this example)
+    $timeLimit = 5;
 
-  // Validate the token based on the time limit
-  if ($timeDiff <= $timeLimit) {
-    // Token is valid within the time limit
+    // Validate the token based on the time limit
+    if ($timeDiff <= $timeLimit) {
+        // Token is valid within the time limit
 
-    // Send a request to https://g82.me/t with the parameter 'p'
-    $url = 'https://g82.me/t';
-    $requestData = array('p' => $p);
+        // Send a request to https://g82.me/t with the parameter 'p'
+        $url = 'https://g82.me/t';
+        $requestData = array('p' => $p);
 
-    $options = array(
-      'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($requestData),
-        'timeout' => 5 // 5 seconds timeout
-      )
-    );
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($requestData),
+                'timeout' => 5 // 5 seconds timeout
+            )
+        );
 
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
 
-    // Set the result in the response
-    $response['status'] = 'success';
-    $response['message'] = $result;
-  } else {
-    // Token has expired
-    $response['status'] = 'error';
-    $response['message'] = 'Token expired';
-  }
+        // Set the result in the response
+        $response['status'] = 'success';
+        $response['message'] = $result;
+    } else {
+        // Token has expired
+        $response['status'] = 'error';
+        $response['message'] = 'Token expired';
+    }
 } else {
-  // Required parameters are missing
-  $response['status'] = 'error';
-  $response['messsage'] = 'Missing required parameters';
+    // Required parameters are missing
+    $response['status'] = 'error';
+    $response['message'] = 'Missing required parameters';
 }
 
 // Send the response as JSON
