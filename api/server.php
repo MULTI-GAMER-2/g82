aac0a9c7e3ef2f74e674d97355093a53849cfbd2
Tech4Ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2023-06-21 12:57:07 +0100

    Update server.php

--- api/server.php
+++ api/server.php
@@ -1,5 +1,11 @@
 <?php
 // Retrieve the token from the POST request
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 $token = isset($_POST['token']) ? $_POST['token'] : null;
 
 // Create a response array
