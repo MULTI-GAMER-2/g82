7d419e0c52c58a519881141a8b76710eea90689a
Tech4Ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2023-06-21 12:39:35 +0100

    Update server.php

--- api/server.php
+++ api/server.php
@@ -1,46 +1,57 @@
 <?php
 // Retrieve the token from the POST request
-$token = $_POST['token'];
+$token = isset($_POST['token']) ? $_POST['token'] : null;
 
-// Extract the timestamp from the token
-$timestamp = substr($token, -13);
-
-// Convert the timestamp to a DateTime object
-$datetime = DateTime::createFromFormat('U', floor($timestamp / 1000));
-
-// Calculate the time difference between the current time and the token's timestamp
-$currentDatetime = new DateTime();
-$timeDiff = $currentDatetime->getTimestamp() - $datetime->getTimestamp();
-
-// Set the time limit in seconds (5 seconds in this example)
-$timeLimit = 5;
-
-// Validate the token based on the time limit
-if ($timeDiff <= $timeLimit) {
-  // Token is valid within the time limit
+// Validate if the token exists and is not empty
+if ($token) {
+  // Extract the timestamp from the token
+  $timestamp = substr($token, -13);
   
-  // Send a request to https://g82.me/t with the parameter 'p'
-  $url = 'https://g82.me/t';
-  $p = $_POST['p'];
-  $data = array('p' => $p);
+  // Convert the timestamp to a DateTime object
+  $datetime = DateTime::createFromFormat('U', floor(intval($timestamp) / 1000));
   
-  $options = array(
-    'http' => array(
-      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-      'method'  => 'POST',
-      'content' => http_build_query($data),
-      'timeout' => 5 // 5 seconds timeout
-    )
-  );
+  // Calculate the time difference between the current time and the token's timestamp
+  $currentDatetime = new DateTime();
+  $timeDiff = $currentDatetime->getTimestamp() - $datetime->getTimestamp();
   
-  $context  = stream_context_create($options);
-  $response = file_get_contents($url, false, $context);
+  // Set the time limit in seconds (5 seconds in this example)
+  $timeLimit = 5;
   
-  // Return the response to the JavaScript code
-  echo $response;
+  // Validate the token based on the time limit
+  if ($timeDiff <= $timeLimit) {
+    // Token is valid within the time limit
+    
+    // Send a request to https://g82.me/t with the parameter 'p'
+    $url = 'https://g82.me/t';
+    $p = getenv('L')
+    
+    if ($p) {
+      $data = array('p' => $p);
+      
+      $options = array(
+        'http' => array(
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query($data),
+          'timeout' => 5 // 5 seconds timeout
+        )
+      );
+      
+      $context  = stream_context_create($options);
+      $response = file_get_contents($url, false, $context);
+      
+      // Return the response to the JavaScript code
+      echo $response;
+    } else {
+      // Parameter 'p' is missing or empty
+      echo 'Missing or empty parameter "p"';
+    }
+  } else {
+    // Token has expired
+    echo 'Token expired';
+  }
 } else {
-  // Token has expired
-  echo 'Token expired';
+  // Token is missing or empty
+  echo 'Missing or empty token';
 }
 ?>
-
