e22808e44185ef8fc3e08a8b5b132d07ba1d6e47
Tech4Ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2023-06-21 13:22:11 +0100

    Update server.php

--- api/server.php
+++ api/server.php
@@ -45,40 +45,47 @@ if (isset($data['token']) && isset($data['p'])) {
     // Convert the timestamp to a DateTime object
     $datetime = DateTime::createFromFormat('U', intval($timestamp) / 1000);
 
-    // Calculate the time difference between the current time and the token's timestamp
-    $currentDatetime = new DateTime();
-    $timeDiff = $currentDatetime->getTimestamp() - $datetime->getTimestamp();
+    // Check if the DateTime object was created successfully
+    if ($datetime !== false) {
+        // Calculate the time difference between the current time and the token's timestamp
+        $currentDatetime = new DateTime();
+        $timeDiff = $currentDatetime->getTimestamp() - $datetime->getTimestamp();
 
-    // Set the time limit in seconds (5 seconds in this example)
-    $timeLimit = 5;
+        // Set the time limit in seconds (5 seconds in this example)
+        $timeLimit = 5;
 
-    // Validate the token based on the time limit
-    if ($timeDiff <= $timeLimit) {
-        // Token is valid within the time limit
+        // Validate the token based on the time limit
+        if ($timeDiff <= $timeLimit) {
+            // Token is valid within the time limit
 
-        // Send a request to https://g82.me/t with the parameter 'p'
-        $url = 'https://g82.me/t';
-        $requestData = array('p' => $p);
+            // Send a request to https://g82.me/t with the parameter 'p'
+            $url = 'https://g82.me/t';
+            $requestData = array('p' => $p);
 
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($requestData),
-                'timeout' => 5 // 5 seconds timeout
-            )
-        );
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'method'  => 'POST',
+                    'content' => http_build_query($requestData),
+                    'timeout' => 5 // 5 seconds timeout
+                )
+            );
 
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
+            $context  = stream_context_create($options);
+            $result = file_get_contents($url, false, $context);
 
-        // Set the result in the response
-        $response['status'] = 'success';
-        $response['message'] = $result;
+            // Set the result in the response
+            $response['status'] = 'success';
+            $response['message'] = $result;
+        } else {
+            // Token has expired
+            $response['status'] = 'error';
+            $response['message'] = 'Token expired';
+        }
     } else {
-        // Token has expired
+        // Invalid token format
         $response['status'] = 'error';
-        $response['message'] = 'Token expired';
+        $response['message'] = 'Invalid token format';
     }
 } else {
     // Required parameters are missing
